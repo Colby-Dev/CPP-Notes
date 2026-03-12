42af8bf74d1fdc11e79b804d750adf57d87f81b5
Colby Fornerette <77943390+Colby-Dev@users.noreply.github.com>
2026-03-12 10:55:12 -0600

    overloading

---
+++ ObjectOriented/Overloading.cc
@@ -0,0 +1,25 @@
+#include <iostream> 
+
+using namespace std; 
+
+/*
+
+----- Overloading (Operator and Function) -----
+
+C++ allows you to specify more than one definition for a function name or an operator in the same scope, which is called function overloading and operator overloading respectively.
+
+An overloaded declaration is a declaration that is declared with the same name as a previously declared delcaration in the same scope, except that both delcarations have different
+arguments and obviously different definition (implementation).
+
+When you call overloaded function or operator, the complier determines the most appropriate definition to use, by comparing the argument types you have used to call the function or
+operator with the parameter types specified in the definitions. The process of selecting the most appropriate overloaded function or operator is called overload resolution.
+
+--- Function Overloading in C++ ---
+
+You can have multiple 
+
+*/
+
+int main(){
+
+}
