b2dfc33f5bc0785daef82fafdfbbf188853cbab1
Colby Fornerette <77943390+Colby-Dev@users.noreply.github.com>
2026-03-17 10:45:53 -0600

    abstraction w example

--- ObjectOriented/Abstraction.cc
+++ ObjectOriented/Abstraction.cc
@@ -42,9 +42,34 @@ In this case whatever programs are using interfaces, they would not be impacted 
 */
 
 #include <iostream> 
+using namespace std;
 
-using namespace std; 
+class Adder{
+    public: 
+        Adder(int i = 0){
+            total = i;
+        }
+
+        void addNum(int number){
+            total += number;
+        }
+
+        int getTotal(){
+            return total;
+        };
+    
+    private: 
+        int total;
+};
 
 int main(){
+    Adder a;
+
+    a.addNum(10);
+    a.addNum(20);
+    a.addNum(30);
+
+    cout << "Total " << a.getTotal() << endl;
+    return 0;
 
 }
