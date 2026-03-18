9de6342407bfbbffb6da0fc48f9dbb693da16749
Colby Fornerette <77943390+Colby-Dev@users.noreply.github.com>
2026-03-18 10:31:09 -0600

    encap

---
+++ ObjectOriented/Encapsulation.cc
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std; 
+
+/*
+
+----- Data Encapsulation -----
+
+> Program statements (code), This is the part of a program that performs actions and they are called functions
+
+> Program data, The data is the information of the program which gets affected by the program functions. 
+
+
+
+
+*/
