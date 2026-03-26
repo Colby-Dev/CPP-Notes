fc8c47e273a8ed7cf99af9244ef211f6fe2781f4
Colby Fornerette <77943390+Colby-Dev@users.noreply.github.com>
2026-03-26 12:01:10 -0600

    Prototype Design

---
+++ DesignPatterns/PrototypeDesign.cc
@@ -0,0 +1,9 @@
+/* 
+
+----- Prototype Design Pattern -----
+
+Consider a scenario where you have an object and your friend asks you to create its replica. How would you do that? You are probably thinking of creating a new object and
+then copying the values from the existing object to the new object. That is some good thinking but, 
+
+
+*/
