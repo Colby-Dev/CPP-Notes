41ecdece3de25a84cdde3571ba1000cad6b1691a
Colby Fornerette <77943390+Colby-Dev@users.noreply.github.com>
2026-04-08 11:30:52 -0600

    struct design

--- DesignPatterns/StructuralDesign.cc
+++ DesignPatterns/StructuralDesign.cc
@@ -2,8 +2,11 @@
 
 ----- Structual Design Pattern -----
 
-The structural design patterns is a way of combining 
+The structural design patterns is a way of combining or arranging different classes and objects to form larger and complex structures to solve a particular problem in a better way.
+These patterns help in simplifying the design by identifying simple ways to realize relationships between entities. 
 
+Take an example of a Building. A building is made up of different parts like roofs, walls, doors, etc. Each part has its own characteristics and functions, but when combined,
+they form a complete structure that serves a specific purpose. Similarly, in software, structual design patterns help in combining different classes and objects to create larger stru
 
 
 */
