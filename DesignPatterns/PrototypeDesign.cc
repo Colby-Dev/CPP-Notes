04a14bed020f02f6a83ba6e44cb64abe53c37e6a
colby <fornerette.colby@gmail.com>
2026-04-07 22:12:13 -0600

    proto EX

--- DesignPatterns/PrototypeDesign.cc
+++ DesignPatterns/PrototypeDesign.cc
@@ -13,9 +13,87 @@ the object without knowing the class of the object. This interface has only one 
 and returning it to the client code. 
 
 Whichever object that supports cloning is called a prototype. If the objects have too many fields and many different types of configurations, then you can just clone them. 
+We can implement the Prototype Pattern in the following two ways: 
 
+> Basic Prototype Pattern, This is the simple way without any registry. 
 
+> Prototype Pattern with Registry, This approach uses a registry to keep track of all prototypes. 
 
+--- Basic Prototype Pattern ---
+
+The following illustration shows the steps for implementing the Basic Prototype Pattern:
+
+    Define Prototype Interface -> Create Concrete Prototype Class -> Client Requests Object -> Cloned Object Returned
 
 
 */
+
+#include <iostream>
+#include <string>
+#include <memory> 
+using namespace std; 
+
+class VirtualMachine { 
+    public: 
+        virtual unique_ptr<VirtualMachine> clone() const = 0;
+        virtual void showConfig() const = 0;
+        virtual ~VirtualMachine() = default;
+
+};
+
+class LinuxVM : public VirtualMachine { 
+    string os; 
+    int ram;
+    int cpu; 
+    public: 
+        LinuxVM(string os, int ram, int cpu) : os(move(os)), ram(ram), cpu(cpu) {}
+
+        unique_ptr<VirtualMachine> clone() const override{ 
+            cout << "Cloning Linux VM..." << endl;
+            return make_unique<LinuxVM>(*this);
+        }
+
+        void showConfig() const override{
+            cout << "Linux VM - OS: " << os
+                 << ", RAM: " << ram << "GB, CPU: " << cpu 
+                 << " cores" << endl; 
+        }
+};
+
+class WindowsVM : public VirtualMachine { 
+    string os; 
+    int ram; 
+    int cpu; 
+    public: 
+        WindowsVM (string os, int ram, int cpu) : os(move(os)), ram(ram), cpu(cpu) {}
+        
+        unique_ptr<VirtualMachine> clone() const override { 
+            cout << "Cloning Windows VM..." << endl; 
+            return make_unique<WindowsVM>(*this);
+        }
+
+        void showConfig() const override{ 
+            cout << "Windows VM - OS: " << os
+                 << ", RAM: " << ram << "GB, CPU: " << cpu
+                 << " cores" << endl;
+        }
+};
+
+int main(){ 
+    unique_ptr<VirtualMachine> linuxVM = make_unique<LinuxVM>("Unbuntu 20.04", 8, 4);
+    unique_ptr<VirtualMachine> windowsVM = make_unique<WindowsVM>("Windows 10", 16, 8);
+
+    cout << "Original VMs: " << endl; 
+    linuxVM->showConfig();
+    windowsVM->showConfig();
+
+    cout << "\nCloning VMs:" << endl;
+    auto clonedLinuxVM = linuxVM->clone();
+    auto clonedWindowsVM = windowsVM->clone();
+
+    cout << "\nCloned VMs: "<< endl;
+    clonedLinuxVM->showConfig();
+    clonedWindowsVM->showConfig();
+
+    return 0;
+};
