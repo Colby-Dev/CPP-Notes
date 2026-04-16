55b28cc2da8eb9a8b7e87e436022113a9c36fa10
colby <fornerette.colby@gmail.com>
2026-04-16 08:49:07 -0600

    facade pattern

---
+++ DesignPatterns/FacadePattern.cc
@@ -0,0 +1,9 @@
+/*
+
+----- Facade Design Pattern -----
+
+Facade Design Pattern is a structural design pattern that provides a single and simplified interface to a complex system of classes, 
+library or framework. It organizes a complicated system by adding a simple interface to it.
+
+
+*/
