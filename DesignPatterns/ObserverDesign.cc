eb36175b8279a580f201cc457beab09ecc158bcd
Colby Fornerette <77943390+Colby-Dev@users.noreply.github.com>
2026-04-20 14:01:16 -0600

    Observer Design Final

--- DesignPatterns/ObserverDesign.cc
+++ DesignPatterns/ObserverDesign.cc
@@ -27,9 +27,95 @@ The observer design pattern consists of the following main components:
 4) Concrete Observer - This is a real, working version of an observer. It knows how to react when the subject tells it about a change. In the weather app, the WeatherDisplay class is a concrete observer
                        because it updates what it shows whenever the weather changes. 
 
+--- Conclusion --- 
+
+In this chapter, you learned about the Observer Design Pattern, how it works, its good and bad sides, and where it is used in real life. This pattern is very helpful for making programs where changes in one
+part need to be shared with many other parts, that keeps everything up to date and easy to manage. 
+
 */
 
 #include <iostream>
 #include <string>
 #include <vector> 
 using namespace std; 
+
+
+class Observer{ 
+    public: 
+        virtual void update(float tempature, float humidity, float pressure) = 0;
+};
+
+class WeatherDisplay : public Observer { 
+    private: 
+        float tempature;
+        float humidity;
+        float pressure; 
+
+    public: 
+        void update(float temp, float hum, float pres) override { 
+            tempature = temp;
+            humidity = hum;
+            pressure = pres;
+            display();
+        }
+        void display() { 
+            cout << "Weather Update - Temperature: " << tempature
+                 << ", Humidity: " << humidity
+                 << ", Pressure: " << pressure << endl;
+        }
+};
+
+// Subject interface
+class Subject {
+    public: 
+        virtual void attach(Observer* observer) = 0;
+        virtual void detach(Observer* observer) = 0;
+        virtual void notify() = 0;
+};
+
+// Concrete Subject class
+class WeatherData : public Subject {
+    private: 
+        vector<Observers*> observers;
+        float tempature;
+        float humidity;
+        float pressure;
+
+    public: 
+        void attach(Observer* observer) override{
+            observers.push_back(observer);
+        }
+        void detach(Observer* observer) override{
+            observers.erase(remove(observers.begin(), observers.end(), observer), observer.end());
+        }
+        void notify() override {
+            for (Observer* observer : observers){
+                observer->update(tempature, humidity, pressure);
+            }
+        }
+        void setMeasurements(float temp, float hum, float pres){
+            tempature = temp;
+            humidity = hum;
+            pressure = pres; 
+            notify();
+        }
+};
+
+int main(){
+    WeatherData weatherData;
+
+    WeatherDisplay display1;
+    WeatherDisplay display2;
+
+    weatherData.attach(&display1);
+    weatherData.attach(&display2);
+
+    weatherData.setMeasurements(112.1, 43.2, 12.2);
+    weatherData.setMeasurements(151.1, 28.1, 32.2);
+
+    weatherData.detach(&display1);
+    weatherData.deatch(&display2);
+
+    return 0;
+
+}
