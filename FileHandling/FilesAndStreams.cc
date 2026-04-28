8bb982dfe015f2e14e64f139d9a894bac4d3ee2f
colby <fornerette.colby@gmail.com>
2026-04-28 11:11:04 -0600

    files ex

--- FileHandling/FilesAndStreams.cc
+++ FileHandling/FilesAndStreams.cc
@@ -79,5 +79,36 @@ using namespace std;
 int main () { 
     char data[100];
 
-    // open
+    // open a file
+    ofstream outfile;
+    outfile.open("afile.dat");
+
+    cout << "Writing to file" << endl;
+    cout << "Enter your name: ";
+    cin.getline(data, 100);
+
+    outfile << data << endl;
+
+    cout << "Enter your age: ";
+    cin >> data;
+    cin.ignore();
+
+    outfile << data << endl;
+
+    outfile.close();
+
+    fstream infile;
+    infile.open("afile.dat");
+    
+    cout << "Reading from the file" << endl;
+    infile >> data;
+
+    cout << data << endl;
+
+    infile >> data;
+    cout << data << endl;
+
+    infile.close();
+
+    return 0;
 }
