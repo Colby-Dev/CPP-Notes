35dda9cb7e199fcb14bb6f44fe3155f7857736e3
Colby <fornerette.colby@gmail.com>
2026-05-15 12:01:20 -0400

    Move semantics with operator

--- Advanced/MoveSemantics.cc
+++ Advanced/MoveSemantics.cc
@@ -25,7 +25,189 @@ An expression in C++ means any valid combination of variable, operator, constant
 
  > rvalue Expression, an rvalue is a type of expression that does not have a memory address and it represents a value that generally appears on the right side. It is a temporary expression that is about to get destroyed. Example of rvalue can be a constant, temporary object, and many more. 
 
- an rvalue reference 
+an rvalue reference is used to reference rvalues or temporary objects. It is denoted by (&&) and is used for implementing move semantics. 
 
+(see Example 1)
+
+--- Why do Move Semantics Apply to Rvalues Only? ---
+
+The move semantics is applied only to rvalues because the rvalues are temporary objects, that are about to get destroyed. The rvalue will not affect the program in future as they are temporary and can be changed. So, applying move semantics on rvalue to transfer the ownership won't affect the program. 
+
+--- Implementing Move Semantics: Move Constructor ---
+
+A move constructor is a special constructor used to transfer the ownership of resources from a temporary object or rvalue to a new object using rvalue reference. The move constructor is automatically called when initializing an object with an rvalue. 
+
+(see Example 3)
+
+--- Move Assignment Operator ---
+
+The move assignment operator uses '(=)' and rvalue reference for move semantic. First, it releases the current resource of object and then take ownership of the source object's resources. 
+
+(see Example 4)
 
 */
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+// ----- Example 1 -----
+int Ex1(){
+	// x is an lvalue
+	int x = 10;
+	
+	// x + 5 is an rvalue
+	int y = x + 5;
+
+	cout << "Old x: " << x << endl;
+	cout << "Old y: " << y << endl;
+
+	// Demonstrating assignments
+	x = 20; // this would be an lvalue, rvalues are not assignables
+	
+	cout << "New x: " << x << endl;
+
+}
+
+// ----- Example 2 -----
+void printValue(int &x){
+	cout << "Calling with Lvalue reference: " << x << endl; // lvalue reference
+}
+
+void printValue(int &&x){
+	cout << "Calling with Rvalue reference: " << x << endl; // rvalue reference 
+}
+
+int Ex2(){
+	int a = 10;
+
+	printValue(a); // a is an lvalue
+	printValue(20); // 20 is an rvalue
+
+	return 0;
+}
+
+// ----- Example 3 -----
+class MyString { 
+	private:
+		char *data;
+		size_t length;
+
+	public:
+
+		// Regular constructor
+		MyString(const char *str){
+
+			length = strlen(str);
+			data = new char[length + 1];
+			strcpy(data, str);
+			cout << "Constructor called\n";
+		}
+
+		// Move constructor
+		MyString(MyString &&other) noexcept {
+
+			data = other.data; // transferring ownership
+			length = other.length;
+			other.data = nullptr;
+			other.length = 0;
+			cout << "Move constructor called\n";
+		}
+
+		// Destructor
+		~MyString(){
+			delete[] data;
+			cout << "Destructor called\n";
+		}
+
+		void print() const {
+			if(data)
+			   cout << data << endl;
+		}
+};
+
+// Function returning a temporary object(rvalue)
+MyString createString(){
+	return MyString("Temporary");
+}
+
+int Ex3(){
+
+	// Calling move constructor
+	MyString s1 = createString();
+	s1.print();
+
+	return 0;
+}
+
+// ----- Example 4 -----
+
+class MyString2 {
+	private:
+		char *data;
+		size_t length;
+
+	public: 
+		// Regular constructor
+		MyString(const char* str){
+			length = strlen(str);
+			data = new char[length + 1];
+			strcpy(data, str);
+			cout << "Constructor called\n";
+		}
+
+		// Move constructor
+		MyString(MyString&& other) noexcept
+		{
+
+		data = other.data;
+		length = other.length;
+		other.data = nullptr;
+		other.length = 0;
+		cout << "Move constructor called\n";
+	
+		}
+		
+		MyString& operator = (MyString&& other) noexcept {
+			if (this != &other) { 
+				delete[] data;
+				data = other.data; // transferring ownership
+				length = other.length;
+				other.data = nullptr;
+				other.length =0;
+				cout << "Move assignment operator called\n";
+			}
+			return *this;
+		}
+
+		// Destructor
+		~MyString() {
+			delete[] data;
+			cout << "Destructor called\n";
+		}
+
+		void print() const {
+			if (data)
+				cout << data << endl;
+		}
+};
+
+int Ex4(){
+	MyString s1("Hello");
+	MyString s2("World");
+
+	cout << "Before Move Assignment: \n";
+	s1.print();
+	s2.print();
+
+	s1 = std::move(s2); // Move assignment operator called
+	
+	cout << "After move assignment: \n;
+	s1.print();
+	s2.print(); //s2 is now empy
+	
+	return 0;
+}
+
+				
+	
